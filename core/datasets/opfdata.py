4f7ad5a687bb8b034652c0abfc8fb061e3d4c1ce
alvaro <alvarocg@mit.edu>
2025-03-28 17:36:56 -0400

    Fixed typos where the input variables were not actually being used

--- core/datasets/opfdata.py
+++ core/datasets/opfdata.py
@@ -21,12 +21,12 @@ class OPFData(OPFDataset):
         self.root = os.path.join("data", "opfdata")
         super().__init__(
             root=self.root,
-            split="train",
-            case_name="pglib_opf_case14_ieee",
-            num_groups=1,
-            topological_perturbations=False,
-            transform=None,
-            pre_transform=None,
-            pre_filter=None,
-            force_reload=False,
+            split=split,
+            case_name=case_name,
+            num_groups=num_groups,
+            topological_perturbations=topological_perturbations,
+            transform=transform,
+            pre_transform=pre_transform,
+            pre_filter=pre_filter,
+            force_reload=force_reload,
         )
