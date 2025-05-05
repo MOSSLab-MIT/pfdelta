4717e3832408311ff9c7d8dc6b6f8e0a8e91651b
Ana Rivera <akrivera@mit.edu>
2025-05-05 13:10:28 -0400

    fixed bug with n-2 and only outaging active components

--- data_generation/src/pf_delta_perturbations.jl
+++ data_generation/src/pf_delta_perturbations.jl
@@ -15,6 +15,10 @@ function perturb_topology!(net; method="none")
     original_br_status = Dict(k => net["branch"][k]["br_status"] for k in branch_keys)
     original_gen_status = Dict(k => net["gen"][k]["gen_status"] for k in gen_keys)
 
+    # Only pick from active lines and generators
+    active_branch_keys = [k for k in branch_keys if original_br_status[k] == 1]
+    active_gen_keys = [k for k in gen_keys if original_gen_status[k] == 1]
+
     # while true
         # Reset branches and gen status
         for k in branch_keys
@@ -30,13 +34,13 @@ function perturb_topology!(net; method="none")
         if method == "N-1"
             if random_num < 0.5
                 # Remove a random line
-                line_to_remove = rand(branch_keys)
+                line_to_remove = rand(active_branch_keys)
                 net["branch"][line_to_remove]["br_status"] = 0
             else
                 # Remove a non-slack generator
-                gen_to_remove = rand(gen_keys)
+                gen_to_remove = rand(active_gen_keys)
                 while net["gen"][gen_to_remove]["index"] in slack_bus_gen_idx
-                    gen_to_remove = rand(gen_keys)
+                    gen_to_remove = rand(active_gen_keys)
                 end
                 net["gen"][gen_to_remove]["gen_status"] = 0
             end
@@ -44,17 +48,17 @@ function perturb_topology!(net; method="none")
         elseif method == "N-2"
             if random_num < 1/3
                 # Remove two random lines
-                selected_lines = Random.shuffle(branch_keys)[1:2]
+                selected_lines = Random.shuffle(active_branch_keys)[1:2]
                 for key in selected_lines
                     net["branch"][key]["br_status"] = 0
                 end
-            elseif random_num > 1/3
+            elseif random_num > 2/3
                 # Remove two random generators (non-slack only)
-                selected_gens = Random.shuffle(gen_keys)[1:2]
+                selected_gens = Random.shuffle(active_gen_keys)[1:2]
                 gen_indices = [net["gen"][g]["index"] for g in selected_gens]
 
                 while any(i -> i in slack_bus_gen_idx, gen_indices)
-                    selected_gens = Random.shuffle(gen_keys)[1:2]
+                    selected_gens = Random.shuffle(active_gen_keys)[1:2]
                     gen_indices = [net["gen"][g]["index"] for g in selected_gens]
                 end
 
@@ -63,12 +67,12 @@ function perturb_topology!(net; method="none")
                 end
             else
                 # Remove one line and one non-slack generator
-                line_to_remove = rand(branch_keys)
+                line_to_remove = rand(active_branch_keys)
                 net["branch"][line_to_remove]["br_status"] = 0
 
-                gen_to_remove = rand(gen_keys)
+                gen_to_remove = rand(active_gen_keys)
                 while net["gen"][gen_to_remove]["index"] in slack_bus_gen_idx
-                    gen_to_remove = rand(gen_keys)
+                    gen_to_remove = rand(active_gen_keys)
                 end
                 net["gen"][gen_to_remove]["gen_status"] = 0
             end
@@ -129,6 +133,7 @@ function perturb_load!(net, r)
     n_loads = length(pd0)
 
     success = false
+    pd_new = similar(pd0)
     while !success
         # Sample uniformly from a ball around pd0
         pd_new = sample_ball(pd0, r)
