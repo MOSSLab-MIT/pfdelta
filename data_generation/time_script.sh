743c0db85d52acde8ab6a906b613dea7a24ae9e5
Alvaro Carbonero <alvarocg@mit.edu>
2025-05-08 07:54:39 -0400

    Improved for large parallel processing scripts

--- data_generation/time_script.sh
+++ data_generation/time_script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=time_benchmark
-#SBATCH --time=1-00:00:00
-
-
-# #SBATCH --cpus-per-task=8
+#SBATCH --job-name=n2_e_57
+#SBATCH --time=4-00:00:00
+#SBATCH --cpus-per-task=24
+#SBATCH --output=%x-%j.out
 
 source ~/.bashrc
 
-# srun julia -p 7 main.jl algorithm30
-julia main.jl linear case14 n-2
+srun julia -p 23 main.jl parallel case57 n-2
+# julia main.jl linear case14 n-2
