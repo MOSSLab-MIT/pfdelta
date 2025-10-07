688f71e8ba053fc7aa668bb6620ad8b656be197b
Alvaro Carbonero <alvarocg@mit.edu>
2025-10-07 13:37:20 -0400

    Changed the script with most updated format

--- data_generation/script_parallel.sh
+++ data_generation/script_parallel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=n_2000
-#SBATCH --time=4-00:00:00
+#SBATCH --job-name=n1_2000_nochpt
+#SBATCH --time=7-00:00:00
 #SBATCH --cpus-per-task=72
 #SBATCH --output=%x-%j.out
 
 source ~/.bashrc
 
-srun julia -p 71 main.jl parallel case2000 none checkpoint
+srun julia -p 71 main.jl parallel case2000 n-1
