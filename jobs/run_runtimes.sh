5c638ae00f0c22834ed73949882a2f85eaae865e
Ana K. Rivera <akrivera@mit.edu>
2025-10-22 18:00:08 -0400

    changes to run close2inf runtimes

--- jobs/run_runtimes.sh
+++ jobs/run_runtimes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=runtimes_case2000_warm
-#SBATCH --time=24:00:00
+#SBATCH --job-name=runtimes_case57
+#SBATCH --time=10:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --output=%x-%j.out
 
-julia main_runtimes.jl case2000 false
+julia scripts/main_runtimes.jl case57 true
