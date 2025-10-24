b1bfed1d7336d042ce380d1da6bea3d60a337593
a-bhagava <abhagava@mit.edu>
2025-10-24 01:50:20 -0400

    Delete jobs directory

--- jobs/run_runtimes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=runtimes_case500_analysis
-#SBATCH --time=10:00:00
-#SBATCH --cpus-per-task=1
-#SBATCH --output=%x-%j.out
-
-julia scripts/main_runtimes.jl case500 true
